27a693903f4d12e98b4d4a0820150ab98a726599
Christian Camilo Beltran <cambelf.dev@gmail.com>
2026-08-04 22:05:44 -0500

    Add React Course, Udemy : Fernando Herrera

---
+++ 01_Frontend/React/01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+let firstName: string = 'Christian';
+const lastName = "Beltran";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLeterR = lastName.includes("r");
+console.log({containsLeterR, diceNumber, firstName});
+lastName.includes("R");
+
+lastName.includes("h");
---
+++ 01_Frontend/React/01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,12 @@
+let firstName: string = 'Christian';
+const lastName = "O'Neal";
+const lastName2 = 'O\'Neal "es el apellido de alguien"'
+
+console.log('====================================');
+console.log({firstName, lastName, lastName2 });
+console.log('====================================');
+
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
